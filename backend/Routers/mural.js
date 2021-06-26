9b39387c731dbeb9686ccb918bfd7777ea8f9096
batflarrow <dwivedia2001@gmail.com>
2021-06-27 03:48:47 +0530

    allow flipbook in comment

--- backend/Routers/mural.js
+++ backend/Routers/mural.js
@@ -38,9 +38,7 @@ router.post('/api/createmural', auth, async (req, res) => {
         res.status(400).json({ msg: 'Something went wrong' });
     }
 });
-router.patch('/api/likemural', auth, async (req, res) => {
-
-
+router.patch('/api/likemural/', auth, async (req, res) => {
     // console.log(req.user);
     try {
         const _id = req.body.muralId;
@@ -58,7 +56,7 @@ router.patch('/api/likemural', auth, async (req, res) => {
         res.status(400).json({ msg: 'Something went wrong' });
     }
 });
-router.patch('/api/unlikemural', auth, async (req, res) => {
+router.patch('/api/unlikemural/', auth, async (req, res) => {
 
     try {
         const _id = req.body.muralId;
@@ -80,18 +78,34 @@ router.post('/api/commentonmural/:id', auth, async (req, res) => {
             return res.status(404).send();
         }
         const mural = await Mural.findById(_id);
-        const commentMural = new Mural(
-            {
-                content: req.body.content,
-                creatorId: req.user._id,
-                creatorUsername: req.user.username,
-                isComment: true
-            });
+        let commentMural;
+        if (req.body.flipbook) {
+            commentMural = new Mural(
+                {
+                    content: req.body.content,
+                    creatorId: req.user._id,
+                    creatorUsername: req.user.username,
+                    flipbook: {
+                        frames: req.body.flipbook.frames,
+                        duration: req.body.flipbook.duration
+                    },
+                    isComment: true
+                });
+        }
+        else {
+            commentMural = new Mural(
+                {
+                    content: req.body.content,
+                    creatorId: req.user._id,
+                    creatorUsername: req.user.username,
+                    isComment: true
+                });
+        }
         const commentMuralResponsive = await commentMural.save();
         const comments = mural.comments.concat({ muralCommentId: commentMuralResponsive._doc._id });
         mural.comments = comments;
         const response = await mural.save();
-        res.status(200).json({ msg: 'Commented On Mural' });
+        res.status(200).json({ msg: 'Commented On Mural', mural: commentMuralResponsive._doc });
     }
     catch (error) {
         console.log(error);
@@ -142,6 +156,7 @@ router.get('/api/commentsonmural/:id', auth, async (req, res) => {
             {
                 $project: {
                     _id: 1, imageUrl: "$content", creatorUsername: 1, creatorId: 1, likedCount: { $size: "$likes" }, commentCount: { $size: "$comments" },
+                    flipbook: 1,
                     isLiked: { $in: [req.user.username, "$likes.likedByUserName"] }
                 }
             },
